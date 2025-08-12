fec8ecc3397aae46f28545a3d9709b22f082526d
Wellikson <48630524+Wellikson@users.noreply.github.com>
2025-08-12 16:47:29 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tarefas = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", tarefas=tarefas)
+
+@app.route("/add", methods=["POST"])
+def add():
+    nome = request.form.get("atividade")
+    status = request.form.get("status")
+    if nome:
+        tarefas.append({"atividade": nome, "status": status})
+    return redirect("/")
+
+@app.route("/update/<int:index>", methods=["POST"])
+def update(index):
+    novo_status = request.form.get("status")
+    if 0 <= index < len(tarefas):
+        tarefas[index]["status"] = novo_status
+    return redirect("/")
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    if 0 <= index < len(tarefas):
+        tarefas.pop(index)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
